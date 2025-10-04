1895f10f16c53aaea7998f7cd7975715fc490c31
Tamil Arasan <tamilarasan2910@gmail.com>
2025-10-04 12:03:04 +0530

    Initial commit - Spring Boot app

---
+++ src/main/java/com/example/productservice01/Productservice01Application.java
@@ -0,0 +1,13 @@
+package com.example.productservice01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Productservice01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Productservice01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/productservice01/Productservice01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productservice01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Productservice01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
